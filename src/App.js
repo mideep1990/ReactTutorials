e227cbb76c5f1369ebdf0d4f0813feb04818029c
mideep1990 <mideepankar@gmail.com>
2023-03-03 23:08:06 +0530

    timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const inputElement = useRef(null);
+  const [counter,setCounter] = useState(0)
+  const [minSec,setminSec] = useState("00:00")
+  useEffect(() => {
+    setTimeout(() => {
+      if(counter>=0)
+      {
+       let minutes = Math.floor(counter / 60);
+       let seconds = counter - minutes * 60;
+       minutes = minutes < 10 ? '0'+minutes : minutes
+       seconds = seconds < 10 ? '0'+seconds : seconds
+       setminSec(minutes+":"+seconds)
+       setCounter(counter-1);
+      }
+      console.log(counter)
+    }, 1000);
+  },[counter]);
+  const start = () => {
+    let min = inputElement.current.value
+    let sec = min * 60
+    setCounter(sec)
+  };
+  const reset = () => {
+    inputElement.current.value=""
+    setCounter(0)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br/><br/>
+      <div>
+      <input type='text' ref={inputElement} 
+      placeholder='Enter time in mins'/>
+      <button onClick={() => start()}>
+        Start
+      </button>
+      <button onClick={() => reset()}>
+        Reset
+      </button>
+      </div>
+      <h2>{minSec}</h2>
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/components/Testcomp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useMachine } from '@xstate/react';
+import { createMachine } from 'xstate';
+
+function Testcomp() {
+ //JS functions and Objects / variables
+
+  return (
+    <>
+      <span>test</span>
+    </>
+  )
+}
+
+export default Testcomp
